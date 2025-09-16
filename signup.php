fade1cec8fd4754fa4038c346c1d1b81f44d22b4
JasonKuria <kuriajason32@gmail.com>
2025-09-16 16:36:21 +0300

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,8 @@
+signup.php
+<?php
+require 'ClassAutoLoad.php';
+$ObjLayout->header($conf);
+$ObjLayout->nav($conf);
+$ObjLayout->banner($conf);
+$ObjLayout->form_frame($conf, $ObjForm);
+$ObjLayout->footer($conf);
